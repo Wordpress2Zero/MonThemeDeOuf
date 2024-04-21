491a56293e9f2311808cfd2a1d9d931b6844a712
alanchalevrai <alanchalevrai@gmail.com>
2024-04-21 11:48:22 +0400

    Épisode 3 - Création du template qui affiche un article

---
+++ wp-content/themes/Mon-ThemeDeOuf/single.php
@@ -0,0 +1,10 @@
+<?php get_header() ?>
+
+    
+    <div id="content">
+        <h1>Un article</h1>
+        <p>C'est un début simple, mais vous pouvez commencer à construire votre thème à partir d'ici.</p>
+    </div>
+    
+
+<?php get_footer(); ?>
